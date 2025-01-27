df6af3d7461b6d202b4835538fac83ac32cf474f
krezx <jotazocarvajal@gmail.com>
2025-01-27 13:48:04 -0300

    Initial Commit

---
+++ CaminaConmigo/CaminaConmigoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaminaConmigoApp.swift
+//  CaminaConmigo
+//
+//  Created by a on 27-01-25.
+//
+
+import SwiftUI
+
+@main
+struct CaminaConmigoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
