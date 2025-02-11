443db362c54a3b48a5d7dc79ff96f6012ae080fb
krezx <jotazocarvajal@gmail.com>
2025-02-11 13:00:11 -0300

    Remove notification service and related configurations

--- CaminaConmigo/App/CaminaConmigoApp.swift
+++ CaminaConmigo/App/CaminaConmigoApp.swift
@@ -31,7 +31,6 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
-        NotificationService.shared.requestAuthorization()
         return true
     }
   
