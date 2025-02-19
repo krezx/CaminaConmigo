0b8b5c53fa4fc9e69c119bbccf9f0621eb694e83
krezx <jotazocarvajal@gmail.com>
2025-02-19 18:08:27 -0300

    Add initial unit test setup for CaminaConmigo app

---
+++ CaminaConmigoTests/CaminaConmigoTests.swift
@@ -0,0 +1,16 @@
+//
+//  CaminaConmigoTests.swift
+//  CaminaConmigoTests
+//
+//  Created by a on 19-02-25.
+//
+
+import Testing
+
+struct CaminaConmigoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
